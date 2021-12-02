0036dce97bbabd25d80dfd847bb96397fc32fe6f
Sergey Batukov <89929657+sashachepuha@users.noreply.github.com>
2021-12-02 22:54:15 +0300

    UPDATE DATA MODELS

--- client/src/data/tasks.js
+++ client/src/data/tasks.js
@@ -2,8 +2,8 @@ export default {
   tasks: [
     {
       id: 1,
-      title: 'Test',
-      description: 'test test test',
+      title: 'Прочти первую статью.',
+      description: 'Необходимо прочитать первую статью, кликни "приступить" для перехода',
       status: 'в работе',
       start: '',
       finish: '',
@@ -12,23 +12,13 @@ export default {
     },
     {
       id: 2,
-      title: 'Test',
-      description: 'test test test',
+      title: 'Пройди тест',
+      description: 'Необходимо выполнить первый тест, кликни "приступить" для перехода',
       status: 'в работе',
       start: '',
       finish: '',
       article: null,
       test: 1,
-    },
-    {
-      id: 3,
-      title: 'Test',
-      description: 'test test test',
-      status: 'в работе',
-      start: '',
-      finish: '',
-      article: 2,
-      tests: null,
     },
   ],
 };
