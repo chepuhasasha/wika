9833cb049ff553424714079381f3e84054a9046e
Sergey Batukov <89929657+sashachepuha@users.noreply.github.com>
2021-12-02 22:01:42 +0300

    add data modeles

---
+++ client/data/tasks.js
@@ -0,0 +1,34 @@
+export default {
+  tasks: [
+    {
+      id: 1,
+      title: 'Test',
+      description: 'test test test',
+      status: 'в работе',
+      start: '',
+      finish: '',
+      articles: [],
+      tests: [],
+    },
+    {
+      id: 2,
+      title: 'Test',
+      description: 'test test test',
+      status: 'в работе',
+      start: '',
+      finish: '',
+      articles: [],
+      tests: [],
+    },
+    {
+      id: 3,
+      title: 'Test',
+      description: 'test test test',
+      status: 'в работе',
+      start: '',
+      finish: '',
+      articles: [],
+      tests: [],
+    },
+  ],
+};
---
+++ client/data/users.js
@@ -0,0 +1,13 @@
+export default {
+  users: [
+    {
+      id: 1,
+      name: 'Батуков С.',
+      specialization: 'Frontend Developer',
+      role: 'Новый сотрудник',
+      courses: [1, 2],
+      tasks: [1],
+      medals: ['👋', '👩‍🚀', '🦸‍♂️'],
+    },
+  ],
+};
