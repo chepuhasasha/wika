dfbab9318bcc88bd14985050458e7a1f40f6875f
David Chakiryan <davidchak@yandex.ru>
2021-12-03 18:12:28 +0300

    COMPLETE [post] /api/project/create

--- api/src/entity/project/project.service.js
+++ api/src/entity/project/project.service.js
@@ -7,10 +7,17 @@ export class ProjectService {
     return await this.repository.model.findByPk(id);
   }
 
-  // async create(obj){
-  //   // return await this.repository.create(obj);
-  //   return null;
-  // }
+  async create(obj){
+    let result = null;
+    let error = null;
+
+    try {
+      result = await this.repository.model.create(obj);
+    } catch (e){
+      error = e;
+    }
+    return {error, result};
+  }
 
   // async delete(obj){
   //   // return await this.repository.create(obj);
