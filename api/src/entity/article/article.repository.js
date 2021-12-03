acccaddd3f19f98bfef0555e2357480ae52411c1
David Chakiryan <davidchak@yandex.ru>
2021-12-03 21:39:08 +0300

    COMPLETE [crud] /api/article

---
+++ api/src/entity/article/article.repository.js
@@ -0,0 +1,48 @@
+import sequelize from 'sequelize';
+
+
+export class ArticleRepository {
+  constructor(dbInstance, loggerInstance){
+    this.logger = loggerInstance;
+    this.model = dbInstance.db.define('Article', {
+      title: {
+        type: sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+
+      likes: {
+        type: sequelize.DataTypes.INTEGER,
+        default: 0,
+      },
+
+      dislikes: {
+        type: sequelize.DataTypes.INTEGER,
+        defaul: 0,
+      },
+
+      elements: {
+        type: sequelize.DataTypes.JSON,
+        allowNull: true
+      }
+      // owner
+      // project
+    }, {
+      modelName: 'acticle',
+      tablename: 'acticles',
+      timestamps: false,
+    });
+
+    this.model.sync({ alter: true })
+      .then(() => {
+        this.logger.info('Table <Article> synchronized successfully')
+      })
+      .catch(err => {
+        this.logger.error(`Table <Article> synchronized with error: ${err}`)
+      })
+  }
+}
