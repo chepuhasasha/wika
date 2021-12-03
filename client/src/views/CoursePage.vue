a0a095a11e20ec0f0d5cb88d3c6d9a9edb48fd02
Sergey Batukov <89929657+sashachepuha@users.noreply.github.com>
2021-12-04 00:53:43 +0300

    course editor

--- client/src/views/CoursePage.vue
+++ client/src/views/CoursePage.vue
@@ -18,6 +18,8 @@ Flex(
   )
     Flex.minicard(gap='5px' wrap)
       button(@click='openProject') к проекту
+      button(v-if='isAdmin' @click='edit = !edit') {{ edit ? 'Отмена ❌' : 'Редактировать ✨' }}
+      button(v-if='edit' @click='save' title='Сохранить') 💾
   Flex.card(
     v-if='cours'
     width='100%'
@@ -28,24 +30,24 @@ Flex(
   )
     h1 {{ cours.title }}
   Flex.card(
-    v-if='tasks'
+    v-if='getTasks'
     col
     width='100%'
     padding='20px'
     gap='10px'
   )
     Task(
-      v-for='(task, i) in tasks'
+      v-for='(task, i) in getTasks'
       :key='i'
       :task='task'
       :num='i'
+      :edit='edit'
     )
+    button(v-if='edit' @click='add' title='Добавить задачу') add +
 </template>
 
 <script>
 import coursesData from '@/data/courses';
-import tasksData from '@/data/tasks';
-// import axios from 'axios';
 
 export default {
   name: 'CoursePage',
@@ -57,9 +59,23 @@ export default {
     return {
       cours: null,
       tasks: null,
+      edit: false,
     };
   },
   computed: {
+    user() {
+      return this.$store.state.user;
+    },
+    isAdmin() {
+      if (this.$store.state.user.role === 'Наставник') {
+        return true;
+      }
+      return false;
+    },
+    getTasks() {
+      console.log(this.cours.tasks);
+      return this.cours.tasks;
+    },
   },
   watch: {
     $route: 'getData',
@@ -73,16 +89,28 @@ export default {
       //     this.course = res.data;
       //   });
       const cours = coursesData.courses.filter(item => +item.id === +this.$route.params.id)[0];
-      const tasks= tasksData.tasks.filter(item => cours.tasks.includes(item.id));
-
+      // const tasks= tasksData.tasks.filter(item => cours.tasks.includes(item.id));
       this.cours = {
         ...cours,
       };
-      this.tasks = tasks
+      // this.tasks = tasks
     },
     openProject() {
       this.$router.push(`/project/${this.cours.project}`);
     },
+    add() {
+      console.log(this.cours.tasks);
+      this.cours.tasks.push({
+        id: 5,
+        title: null,
+        description: null,
+        status: 'в работе',
+        article: null,
+        test: null,
+        course: this.cours.id,
+      });
+    },
+    save() {},
   },
   mounted() {
     this.getData();
